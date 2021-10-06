2cb520d60e1929242dc8b6ecf281abd58d35e742
susumOyaji <sumitomo0210@gmail.com>
2021-10-06 12:37:03 +0900

    up

--- app/src/main/java/customphonedialer/abror96/customphonedialer/DialerActivity.java
+++ app/src/main/java/customphonedialer/abror96/customphonedialer/DialerActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+//import androidx.appcompat.app.AppCompatActivity;
 import android.telecom.TelecomManager;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
